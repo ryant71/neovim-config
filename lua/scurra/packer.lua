8b0298c06d78427688ad6cfdbec5ed0010eebdba
Ryan Tracey <ryan.tracey@gmail.com>
2023-05-12 14:08:15 +0200

    Partial commit

--- lua/scurra/packer.lua
+++ lua/scurra/packer.lua
@@ -9,9 +9,9 @@ return require("packer").startup(function(use)
 
     -- begin theprimegean --
     use {
-	    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	    -- or                            , branch = '0.1.x',
-	    requires = { {'nvim-lua/plenary.nvim'} }
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
     }
 
     use({
@@ -28,23 +28,23 @@ return require("packer").startup(function(use)
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
     use {
-	    'VonHeikemen/lsp-zero.nvim',
-	    branch = 'v2.x',
-	    requires = {
-		    -- LSP Support
-		    {'neovim/nvim-lspconfig'},             -- Required
-		    {                                      -- Optional
-		    	'williamboman/mason.nvim',
-		    	run = function()
-			    pcall(vim.cmd, 'MasonUpdate')
-		    	end,
-	        },
-	        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-	        -- Autocompletion
-	        {'hrsh7th/nvim-cmp'},     -- Required
-	        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	        {'L3MON4D3/LuaSnip'},     -- Required
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
         }
     }
 
@@ -55,9 +55,12 @@ return require("packer").startup(function(use)
 
     use("folke/zen-mode.nvim")
 
-    -- -- colors
-    use("folke/tokyonight.nvim")
-    use("gruvbox-community/gruvbox")
+    use("folke/tokyonight.nvim")     -- color
+    use("gruvbox-community/gruvbox") -- color
+    use("sainnhe/everforest")        -- color
+    use("sainnhe/gruvbox-material")  -- color
+    use("sainnhe/sonokai")           -- color
+    -- use("x/rosepine")             -- color
 
     -- I gave up on opening nvim in last cursor position
     -- so I have resorted to a plugin
